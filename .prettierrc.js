dd10f6722bedc8177a0ba063c60e053b6c4dd1b6
Ayesha Iftikhar <ayesha.iftikhar@arbisoft.com>
2020-11-25 20:56:31 +0500

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  // jsxSingleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  printWidth: 100,
+};
